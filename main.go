dc907560cacaf291925d3da61d0b08f134451a46
yongkangchia <yong.kangchia@smartcontract.com>
2024-08-13 11:30:07 +0800

    removed comments

--- main.go
+++ main.go
@@ -10,7 +10,6 @@ import (
 )
 
 func encodeBalanceOfParam(address string) (string, error) {
-	// Convert Base58 address to hex
 	tronAddress, err := tronaddress.Base58ToAddress(address)
 	if err != nil {
 		return "", fmt.Errorf("failed to convert Base58 address to hex: %v", err)
@@ -19,30 +18,24 @@ func encodeBalanceOfParam(address string) (string, error) {
 	testTronAddress := tronaddress.HexToAddress("0x41a614f803b6fd780986a42c78ec9c7f77e6ded13c")
 	fmt.Println("Test Tron address:", testTronAddress.String())
 
-	// Remove the "41" prefix if present
 	address = tronAddress.Hex()
 	// address = strings.TrimPrefix(address, "0x41")
 	fmt.Println("Hex address:", address)
 
-	// Convert the address to the appropriate type
 	ethAddress := common.HexToAddress(address)
 
-	// Create a simple ABI with just the balanceOf function
 	const abiJSON = `[{"name":"balanceOf","type":"function","inputs":[{"name":"","type":"address"}]}]`
 
-	// Parse the ABI
 	parsedABI, err := abi.JSON(strings.NewReader(abiJSON))
 	if err != nil {
 		return "", fmt.Errorf("failed to parse ABI: %v", err)
 	}
 
-	// Encode the parameter
 	data, err := parsedABI.Pack("balanceOf", ethAddress)
 	if err != nil {
 		return "", fmt.Errorf("failed to encode parameter: %v", err)
 	}
 
-	// Convert to hex string and remove the function selector (first 4 bytes)
 	return common.Bytes2Hex(data[4:]), nil
 }
 
